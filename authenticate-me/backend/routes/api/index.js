eaeb651a480de0fd73ace28dbd3a3bdfa8175997
Dylan Silva <rogdylan98@gmail.com>
2021-12-05 18:49:12 -0600

    auth part 1 phase 3

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { User } = require('../../db/models');
+
+
+//test errors route
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
